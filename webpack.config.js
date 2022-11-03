7e9e64da3bd7b33b2a57431745bf8ba720610831
jingyanzhong <106657969+jingyanzhong@users.noreply.github.com>
2022-11-03 16:47:21 +0800

    1103修改新增

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mode: 'development', 
+    entry: './js/all.js', // webpack 打包的起始檔案
+    output: {
+      path: __dirname, // 在這個 config 檔同層的目錄
+      filename: 'bundle.js'// 打包輸出的檔名
+    }
+  }
