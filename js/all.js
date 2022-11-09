83d1a17ac92a2fafcf23a91192946b8903fd7c0c
jingyanzhong <106657969+jingyanzhong@users.noreply.github.com>
2022-11-09 15:19:21 +0800

    1109新增贊助商動畫、細節修正

--- js/all.js
+++ js/all.js
@@ -1,6 +1,6 @@
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger.js";
-import { TextPlugin } from "gsap/TextPlugin.js";
+import { gsap } from "../node_modules/gsap/all.js";
+import { ScrollTrigger } from "../node_modules/gsap/ScrollTrigger.js";
+import { TextPlugin } from "../node_modules/gsap/TextPlugin.js";
 
 gsap.registerPlugin(ScrollTrigger, TextPlugin);
 
@@ -25,7 +25,7 @@ gsap.utils.toArray(".animation-wrapper").forEach((element) => {
 
     ScrollTrigger.create({   //每一個元素遍歷都會創造對應的ScrollTrigger物件
      trigger: element,
-      markers: true,
+      markers: false,
       onEnter: function () {
         animated(element); 
       },
@@ -116,15 +116,6 @@ function hide(element) {
   gsap.set(element, { opacity: 0, visibility: "hidden" });
 }
 
-gsap.to(".text", {
-      text: "這裡是第一段", //text屬性將自動為DOM元素嵌入我們所輸入的文字
-      duration: 1.5,
-      scrollTrigger: {
-				trigger: ".text",
-        toggleActions: "play pause resume reset", //見備註
-      },
-    });
-
 gsap.fromTo(
   ".cursor",
   0,
@@ -138,3 +129,152 @@ gsap.fromTo(
     repeatDelay: 0.3, // 下一次repeat的delay時間
   }
 );
+
+// 互動式網頁設計
+const mql = window.matchMedia("(max-width:768px)"); 
+const scrollTL1 = gsap.timeline({
+  scrollTrigger:{
+    trigger:".section2-bg",
+    pin: true,
+    markers: false,
+    scrub: true,
+  }
+});
+scrollTL1.to(".box-bg-1",{opacity: "0"});
+scrollTL1.to(".title",{
+  yPercent:'-80',
+},"<")
+scrollTL1.to(".bg-1",{
+  yPercent:'10',
+},"<")
+scrollTL1.to(".bg-2",{
+  yPercent:'50',
+},"<")
+scrollTL1.to(".bg-3",{
+  yPercent:'50',
+},"<")
+scrollTL1.to(".box-bg-2",{opacity: "0"});
+scrollTL1.to(".title",{
+  yPercent:'-100',
+},"<")
+scrollTL1.to(".bg-1",{
+  yPercent:'50',
+},"<")
+scrollTL1.to(".bg-2",{
+  yPercent:'100',
+},"<")
+scrollTL1.to(".bg-3",{
+  yPercent:'100',
+},"<")
+if(mql.matches){
+  scrollTL1.to(".box-bg-1",{opacity: "0"});
+  scrollTL1.to(".title",{
+    yPercent:'-150',
+  },"<")
+  scrollTL1.to(".bg-1",{
+    yPercent:'10',
+  },"<")
+  scrollTL1.to(".bg-2",{
+    yPercent:'50',
+  },"<")
+  scrollTL1.to(".bg-3",{
+    yPercent:'50',
+  },"<")
+  scrollTL1.to(".box-bg-2",{opacity: "0"});
+  scrollTL1.to(".title",{
+    yPercent:'-300',
+  },"<")
+  scrollTL1.to(".bg-1",{
+    yPercent:'50',
+  },"<")
+  scrollTL1.to(".bg-2",{
+    yPercent:'100',
+  },"<")
+  scrollTL1.to(".bg-3",{
+    yPercent:'100',
+  },"<")
+}
+
+// 每周活動介紹動畫
+const scrollTL = gsap.timeline({
+  scrollTrigger:{
+    trigger:".section3",
+    pin: true,
+    markers: false,
+    scrub: true,
+  }
+});
+scrollTL.to(".gate-1",{xPercent:"-100"});
+scrollTL.to(".gate-2",{xPercent:"-100"});
+// 活動流程
+const scrollTL4_1 = gsap.timeline({
+  scrollTrigger:{
+    trigger:".step1",
+    markers: false,
+    scrub: true,
+    start: 'top 90%', // 決定動畫開始點的位置
+    end: 'top 30%',
+  }
+});
+scrollTL4_1.from(".section4-icon-hidden1",{opacity:0})
+
+const scrollTL4_2 = gsap.timeline({
+  scrollTrigger:{
+    trigger:".step2",
+    markers: false,
+    scrub: true,
+    start: 'top 90%', // 決定動畫開始點的位置
+    end: 'top 30%',
+  }
+});
+scrollTL4_2.from(".section4-icon-hidden2",{opacity:0})
+const scrollTL4_3 = gsap.timeline({
+  scrollTrigger:{
+    trigger:".step3",
+    markers: false,
+    scrub: true,
+    start: 'top 90%', // 決定動畫開始點的位置
+    end: 'top 30%',
+  }
+});
+scrollTL4_3.from(".section4-icon-hidden3",{opacity:0})
+const scrollTL4_4 = gsap.timeline({
+  scrollTrigger:{
+    trigger:".step4",
+    markers: false,
+    scrub: true,
+    start: 'top 80%', // 決定動畫開始點的位置
+    end: 'top 65%',
+  }
+});
+scrollTL4_4.from(".section4-icon-hidden4",{opacity:0})
+
+// 比賽說明
+
+gsap.to(".section5-text-1",{
+  text: "區區修煉已經無法滿足了嗎？",
+  duration: 1.5,
+  scrollTrigger:{
+    trigger:".section5",
+    toggleActions: "play pause resume reset",
+  }
+});
+
+gsap.to(".section5-text-2",{
+  text: "還有比賽等著你！",
+  duration: 1.5,
+  delay:1.5,
+  scrollTrigger:{
+    trigger:".section5",
+    toggleActions: "play pause resume reset",
+  }
+});
+
+
+// 贊助商
+gsap.to(".loop",{
+  xPercent:"-100",
+  ease:"none",
+  duration:15,
+  repeat:-1
+})
